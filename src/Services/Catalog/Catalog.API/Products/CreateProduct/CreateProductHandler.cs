374be327bd04eb2fc1d184f35ee3451aa54a5543
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-10 21:11:38 +0300

    Make Building Blocks to store All CQRS Dependencies make all interfaces ICommand , IQuery , ICommandHandler , IQueryHandler

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,35 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name , List<string> Category , string Description , string ImageFile , decimal Price)
-        :IRequest<CreateProductResult>;
+        :ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             // Business logic to create a Product
-            throw new NotImplementedException();
+
+
+            // 1- create product entity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            // 2- save the database
+
+            // 3- return CreateProductResult result
+            return new CreateProductResult(Guid.NewGuid());
+
         }
     }
 }
