9938efc7c48176702519b712387b527eb909cd0f
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-10 21:23:05 +0300

    add Carter Package

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
-
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid Id);
     public class CreateProductEndpoint
     {
     }
