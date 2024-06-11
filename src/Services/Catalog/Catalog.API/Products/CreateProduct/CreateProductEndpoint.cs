7e0f5a725969f9a8d22d616f36b4f11557865b36
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-11 17:22:17 +0300

    make Create Product EndPoint

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,4 +1,6 @@
 ﻿using Carter;
+using Mapster;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct
 {
@@ -8,7 +10,20 @@ namespace Catalog.API.Products.CreateProduct
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            throw new NotImplementedException();
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+
+                var command = request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{response.Id}", response);
+
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(statusCode: 201)
+            .ProducesProblem(statusCode: 400)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product Des");
         }
     }
 }
