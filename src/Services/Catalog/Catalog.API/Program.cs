d0a35e8da08a83eb21d9f5c153edc15ac7e2588b
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-13 01:15:22 +0300

    make docker compose to Postgresql DB add Docker image Configration Database

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -5,7 +5,9 @@ builder.Services.AddCarter();
 builder.Services.AddMediatR(config => {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
-
+builder.Services.AddMarten(opts => {
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
