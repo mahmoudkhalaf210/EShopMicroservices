40d3fb56a3ac08f20a1eb0407d3380a869eaf3f2
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-15 19:29:18 +0300

    add Delete Product End Point

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,24 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductRequest(Guid id);
+    public record DeleteProductResponse(bool IsSuccess);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{Id}", async (Guid Id, ISender sender) => {
+
+                var result = await sender.Send(new DeleteProductCommand(Id));
+                var response = result.Adapt<DeleteProductResponse>();
+                return Results.Ok(response);
+            }).WithName("DeleteProduct")
+            .Produces<CreateProductResponse>(statusCode: 201)
+            .ProducesProblem(statusCode: 400)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product Des");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+    public record UpdateProductResponse(bool IsSuccess);
+    public class UpdateProductEndpoint : ICarterModule
+    {
+        void ICarterModule.AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products/", async (UpdateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<UpdateProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("Update Product")
+            .Produces<CreateProductResponse>(statusCode: 201)
+            .ProducesProblem(statusCode: 400)
+            .WithSummary("Update Product")
+            .WithDescription("Update Product Des");
+        }
+    }
+}
