9dcaaeb0139e191207bc9c0790c507a42d7f7c1f
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-16 20:40:09 +0300

    Make Validation Behaviour Class in Building Blocks and inject

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,40 @@
 ﻿
 
+
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name , List<string> Category , string Description , string ImageFile , decimal Price)
         :ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler (IDocumentSession session)
+
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand> 
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is requied");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than Zero");
+
+        }
+    }
+
+    internal class CreateProductCommandHandler (IDocumentSession session , ILogger<CreateProductCommandHandler> logger , IValidator<CreateProductCommand> validator)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             // Business logic to create a Product
+
+            logger.LogInformation(" CreateProductCommandHandler.handle called {@command} ", command);
+
+            var result = await validator.ValidateAsync(command , cancellationToken);
+            var errors = result.Errors.Select(e => e.ErrorMessage).ToList();
+            if (errors.Any())
+            {
+                throw new ValidationException(errors.FirstOrDefault());
+            }
 
 
             // 1- create product entity from command object
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,16 @@
+using BuildingBlocks.Behaviours;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // add services to the container
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config => {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
 });
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
 builder.Services.AddMarten(opts => {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
