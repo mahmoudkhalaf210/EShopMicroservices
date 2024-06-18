4343264a6a3bdc00384d5430409bd018a5de4d7d
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-18 17:02:14 +0300

    delete custom exception handler in program to use Class

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,4 @@
-using BuildingBlocks.Behaviours;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,33 +23,6 @@ var app = builder.Build();
 
 app.MapCarter();
 
-
-app.UseExceptionHandler(exceptionHandlerApp => {
-
-    exceptionHandlerApp.Run(async context =>
-    {
-        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
-        if (exception is null) {
-            return;
-        }
-
-        var proplemDetails = new ProblemDetails
-        {
-            Title = exception.Message,
-            Status = StatusCodes.Status500InternalServerError,
-            Detail = exception.StackTrace,
-        };
-
-        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
-        logger.LogError(exception, exception.Message);
-
-        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        context.Response.ContentType = "application/problem+json";
-
-        await context.Response.WriteAsJsonAsync(proplemDetails);
-    });
-
-});
 
 
 
