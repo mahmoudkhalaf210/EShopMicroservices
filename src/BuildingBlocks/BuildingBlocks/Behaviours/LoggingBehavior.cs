38ae8baf47e1326f902bcfee91d290ae4ec156ac
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-18 17:27:00 +0300

    make LoggingBehavior Pipleine For Cross Cutting Concerns

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviours/LoggingBehavior.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Behaviours
+{
+    internal class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull, IRequest<TResponse>
+        where TResponse : notnull
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("[START] Handle Request = {Request} - Response = {Response} - RequestData = {RequstData}",
+                typeof(TRequest).Name, typeof(TResponse).Name, request);
+
+            var timer = new Stopwatch();
+            timer.Start();
+
+            var response = await next();
+            timer.Stop();
+
+            var timeTaken = timer.Elapsed;
+            if (timeTaken.Seconds > 3) {
+                logger.LogWarning("[PERFOEMANCE] The Request {Request} took {TimeTaken} Seconds", typeof(TRequest).Name, timeTaken);
+            }
+
+            logger.LogInformation("[END] Handled {Request} with {Response}", typeof(TRequest).Name, typeof(TResponse).Name);
+            return response;
+        }
+
+
+    }
+}
