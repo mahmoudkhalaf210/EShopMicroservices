50a0aca9f3813f7417c789312e842357aa4186e2
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-18 16:57:09 +0300

    make execptions Handlers using IExceptionHandler Handle all Exceptions

--- src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -1,12 +1,80 @@
-﻿using System;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BuildingBlocks.Exceptions.Handler
 {
-    internal class CustomExceptionHandler
+    internal class CustomExceptionHandler (ILogger<CustomExceptionHandler> logger)
+        : IExceptionHandler
     {
+        public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+        {
+            logger.LogError("Error Message : {exceptionMessage}, Time of occurance {time}",
+                exception.Message, DateTime.UtcNow);
+
+            (string Detail, string Title, int StatusCode) details = exception switch
+            {
+                InternalServerException =>
+                (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+                ),
+
+                FluentValidation.ValidationException =>
+                (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status400BadRequest
+                ),
+
+                BadRequestException =>
+                (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status400BadRequest
+                ),
+
+                NotFoundException =>
+                (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status404NotFound
+                ),
+                _ =>
+                (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+                )
+            };
+
+
+            var problemDetials = new ProblemDetails
+            {
+                Title = details.Title,
+                Detail = details.Detail,
+                Status = details.StatusCode,
+                Instance = context.Request.Path
+            };
+
+            problemDetials.Extensions.Add("traceId", context.TraceIdentifier);
+
+            if (exception is FluentValidation.ValidationException validationException) {
+                problemDetials.Extensions.Add("ValidationsErrors", validationException.Errors);
+            }
+
+            await context.Response.WriteAsJsonAsync( problemDetials , cancellationToken : cancellationToken );
+            return true;
+
+        }
     }
 }
