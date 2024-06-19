f0a27e43f352a54eee65c4046f8948e77c8da6b2
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-19 16:17:27 +0300

    add Models Of Basket

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; } = default!;
+
+        public List<ShoppingCartItem> Items { get; set; } = new();
+
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+        public ShoppingCart(string username)
+        {
+            UserName = username;
+        }
+
+        public ShoppingCart()
+        {
+            
+        }
+    }
+}
