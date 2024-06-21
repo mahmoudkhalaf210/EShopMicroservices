5e1a53fed71f04bd9637ccd085d34cd9db7dffa8
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-22 00:37:53 +0300

    add DiscountContext and inject in Program

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,15 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext : DbContext
+    {
+        public DiscountContext(DbContextOptions<DiscountContext> options):base(options)
+        {
+               options.
+        }
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+    }
+
+}
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,10 +1,16 @@
 
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var ConnectionDb = builder.Configuration.GetConnectionString("Database");
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountContext>(opts => {
+    opts.UseSqlite(ConnectionDb!);
+});
 
 var app = builder.Build();
 
