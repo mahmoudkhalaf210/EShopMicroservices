2b60e4f8edcbbf4ede5d45d0a30edc854fa857b0
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-22 21:11:58 +0300

    inject service DI add grpcSettings Url

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,41 +1,47 @@
 
 
 
+using Discount.Grpc.Protos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly;
 var connectionDb = builder.Configuration.GetConnectionString("Database");
 var connectionRedisDb = builder.Configuration.GetConnectionString("Redis");
 
-
+// Application Services
 builder.Services.AddMediatR(config => {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 
 });
+builder.Services.AddCarter();
 
-builder.Services.AddValidatorsFromAssembly(assembly);
 
+
+// Data Services
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(connectionDb!);
     opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
-
-
-
 builder.Services.AddScoped<IBasketRepositroy, BasketRepositroy>();
 builder.Services.Decorate<IBasketRepositroy, CachedBasketRepository>();
-
 builder.Services.AddStackExchangeRedisCache(options => {
     options.Configuration = connectionRedisDb;
 });
 
+// Grpc Services 
+builder.Services.AddGrpcClient<discountProtoService.discountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
 
+});
+
+// Cross Cutting Services
+builder.Services.AddValidatorsFromAssembly(assembly);
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
-
-builder.Services.AddCarter();
 builder.Services.AddHealthChecks()
     .AddNpgSql(connectionDb!)
     .AddRedis(connectionRedisDb!);
