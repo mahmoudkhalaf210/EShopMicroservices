8d02323f90e35e8d7af9b62a466fe1427d48eb3a
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-24 07:46:55 +0300

    Make ValueObjects Address Payment

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Ordering.Domain.ValueObjects
+{
+    public record Payment
+    {
+        public string? CardName { get; set; } = default!;
+        public string CardNumber { get; set; } = default!;
+        public string Expiration { get; set; } = default!;
+        public string CVV { get; set; } = default!;
+        public int PaymentMethod { get; set; } = default!;
+
+    }
+}
