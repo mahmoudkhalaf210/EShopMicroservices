e96269e7461695b7f1856abcae1a9c9991da5c47
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-24 07:40:32 +0300

    Make Models Customer Order Orderitem Product

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Ordering.Domain.Abstractions;
+using System.Net;
 
 namespace Ordering.Domain.Models
 {
-    internal class Order
+    public class Order : Aggregate<Guid>
     {
+        private readonly List<OrderItem> _orderItems = new();
+        public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+        public Guid CustomerId { get; private set; } = default!;
+        public Guid CustomerId { get; private set; } = default!;
+        public Address ShippingAddress { get; private set; } = default!;
+        public Address BillingAddress { get; private set; } = default!;
+        public Payment Payment { get; private set; } = default!;
+
+        public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+
+        public decimal TotalPrice
+        {
+            get => OrderItems.Sum(x => x.Price * x.Quantity);
+            private set { }
+        }
+
     }
 }
