b02bc2073f7755b00d81dccebcfa95695268e4af
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-26 12:04:25 +0300

    Add Create Order Endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
@@ -1,6 +1,26 @@
 ﻿namespace Ordering.API.Endpoints
 {
-    public class CreateOrder
+    public record CreateOrderRequest(OrderDto order);
+
+    public record CreateOrderResponse(Guid Id);
+
+    public class CreateOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/orders", async (CreateOrderRequest request, ISender sender) => {
+                var command = request.Adapt<CreateOrderCommand>();
+                var result = sender.Send(command);
+                var response = result.Adapt<CreateOrderResponse>();
+
+                return Results.Created($"/orders/{response.Id}", response);
+            
+            })
+            .WithName("CreateOrder")
+            .Produces<CreateOrderResponse>(statusCode: 201)
+            .ProducesProblem(statusCode: 400)
+            .WithSummary("Create Order")
+            .WithDescription("Create Order Des");
+        }
     }
 }
---
+++ src/Services/Ordering/Ordering.API/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using BuildingBlocks.CQRS;
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Orders.Commands.CreateOrder;
