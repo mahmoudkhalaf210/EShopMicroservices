a0ace9357f3f3c875b2d37b554b36946293f8e04
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-27 00:24:46 +0300

    Add Building Blocker Messaging Add Rabbit mq pakage add events add connection string (Configuration) of Message Broker in Basket

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.Messaging.MassTransit
+{
+    public static class Extentions
+    {
+
+        public static IServiceCollection AddMessageBroker(this IServiceCollection services , Assembly? assembly = null ) 
+        {
+
+            return services;
+        }
+
+    }
+}
