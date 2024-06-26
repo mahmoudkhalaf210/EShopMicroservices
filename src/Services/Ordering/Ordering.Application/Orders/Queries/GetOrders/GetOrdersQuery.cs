47a36c9b2bb9ddd3562d4c8e7483462e0c6d8a54
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-26 11:19:51 +0300

    make Get Orders With Pagination

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,10 @@
+﻿
+using BuildingBlocks.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders
+{
+    public record GetOrdersQuery(PaginatedRequest paginationRequest) : IQuery<GetOrdersResult>;
+    
+    public record GetOrdersResult(PaginatedResult<OrderDto> orders);
+
+}
