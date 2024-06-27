449c0fb9e53d1c093af6f2b35febfbd81ea3a5ae
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-27 16:34:57 +0300

    make configuration basket catalog ordering

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,5 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
+app.MapReverseProxy();
 
 app.Run();
