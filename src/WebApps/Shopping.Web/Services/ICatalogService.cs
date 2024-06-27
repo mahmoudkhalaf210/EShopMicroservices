3f9ed55ae60f994d93b5d7ef65e9691e6db5812f
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-27 18:44:07 +0300

    Download Package Refit.HttpClientFactory

---
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,12 @@
+﻿using Shopping.Web.Models.Catalog;
+
+namespace Shopping.Web.Services
+{
+    public interface ICatalogService
+    {
+        Task<GetProductsResponse> GetProducts(int? PageNumber = 1, int? PageSize = 10);
+        Task<GetProductByIdResponse> GetProduct(Guid id);
+
+        Task<GetProductByCategoryResponse> GetProductByCategory(string category);
+    }
+}
