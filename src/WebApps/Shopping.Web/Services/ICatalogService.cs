8ff71f4afe3615f599beedc1da4a5ab0c6173597
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-27 18:58:11 +0300

    Finish Make ICategory

--- src/WebApps/Shopping.Web/Services/ICatalogService.cs
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -3,9 +3,11 @@ namespace Shopping.Web.Services
 {
     public interface ICatalogService
     {
+        [Get("/catalog-service/products?pageNumber={PageNumber}&pageSize={pageSize}")]
         Task<GetProductsResponse> GetProducts(int? PageNumber = 1, int? PageSize = 10);
+        [Get("/catalog-service/products/{id}")]
         Task<GetProductByIdResponse> GetProduct(Guid id);
-
+        [Get("/catalog-service/products/category/{category}")]
         Task<GetProductByCategoryResponse> GetProductByCategory(string category);
     }
 }
