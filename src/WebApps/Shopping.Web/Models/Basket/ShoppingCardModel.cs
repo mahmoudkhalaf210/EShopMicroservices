bec870d0050af9bc8cce451cb21fb5fea76d1691
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-27 18:10:07 +0300

    make ShoppingCardModel

---
+++ src/WebApps/Shopping.Web/Models/Basket/ShoppingCardModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Shopping.Web.Models.Basket
+{
+    public class ShoppingCardModel
+    {
+        public string UserName { get; set; } = default!;
+
+        public List<ShoppingCardItemModel> Items { get; set; } = new();
+
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+
+    }
+
+    public class ShoppingCardItemModel {
+
+        public int Quantity { get; set; } = default!;
+        public string Color { get; set; } = default!;
+        public decimal Price { get; set; } = default!;
+        public Guid ProductId { get; set; } = default!;
+        public string ProductName { get; set; } = default!;
+    }
+
+
+
+
+}
