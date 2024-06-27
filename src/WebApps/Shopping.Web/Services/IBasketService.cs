051acb131844d6a564af06d76d42a98674a43fb0
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-27 19:05:23 +0300

    make IBasketService

---
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -0,0 +1,24 @@
+﻿using Shopping.Web.Models.Basket;
+
+namespace Shopping.Web.Services
+{
+    public interface IBasketService
+    {
+        [Get("/basket-service/basket/{userName}")]
+        Task<GetBasketResponse> GetBasket(string userName);
+
+        [Get("/basket-service/basket")]
+        Task<StoreBasketResponse> StoreBasket(StorebasketRequest request);
+
+        [Get("/basket-service/basket/{userName}")]
+        Task<DeleteBasketResponse> DeleteBasket(string userName);
+
+
+        [Get("/basket-service/basket/checkout")]
+        Task<CheckoutBasketResponse> checkoutBasket(CheckoutBasketRequest request);
+
+
+
+
+    }
+}
