64a9adcf47dbc2c67d87cf7079bd45939346db63
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-27 20:32:54 +0300

    make IOrderingService

--- src/WebApps/Shopping.Web/Services/IOrderingService.cs
+++ src/WebApps/Shopping.Web/Services/IOrderingService.cs
@@ -1,6 +1,15 @@
-﻿namespace Shopping.Web.Services
+﻿using Shopping.Web.Models.Ordering;
+
+namespace Shopping.Web.Services
 {
     public interface IOrderingService
     {
+        [Get("/ordering-service/orders?pageIndex={pageIndex}&pageSize={pageSize}")]
+        Task<GetOrderResponse> GetOrders(int? pageIndex = 1, int? pageSize = 10);
+        [Get("/ordering-service/orders/{orderName}")]
+        Task<GetOrderByNameResponse> GetOrderByName(string orderName);
+        [Get("/ordering-service/orders/{customerId}")]
+        Task<GetOrderByCustomerResponse> GetOrderByCustomer(Guid customerId);
+
     }
 }
